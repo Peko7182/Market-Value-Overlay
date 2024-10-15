e987e05f4aee87a5e5ce14af66b65f1f48c86e9c
Peko <lukaskotek2@gmail.com>
2024-10-15 17:53:15 +0200

    Less laggy?
    I've made some changes that may help reduce potential lag caused by this code. Although I haven't received any reports of lagging, I wanted to ensure that people with less powerful devices can still have a smooth experience.

--- Source/Overlay.cs
+++ Source/Overlay.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
 using Verse;
 using RimWorld;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace MarketValueOverlay
 {
-    /// <summary>
-    /// Overlay that displays buildings with market value on the map.
-    /// </summary>
     public class Overlay : ICellBoolGiver
     {
         private static readonly Color MinColor = Color.green;
@@ -17,13 +14,11 @@ namespace MarketValueOverlay
         private Color _color;
         private CellBoolDrawer _drawer;
         private int _nextUpdateTick;
-        private float _minValue, _maxValue, _avgValue;
+        private float _minValue, _maxValue, _avgValue; // Market values of buildings, min/max/avg of all buildings
+        private Dictionary<int, float> _buildingValues = new Dictionary<int, float>(); // Market values of each building
 
-        /// <summary>
-        /// Dummy color for the overlay.
-        /// </summary>
         public Color Color => Color.white;
-
+        
         /// <summary>
         /// Updates the overlay with the specified update delay.
         /// </summary>
@@ -32,18 +27,21 @@ namespace MarketValueOverlay
         {
             if (!MarketValueOverlayMod.OverlayEnabled || !MarketValueOverlayMod.Settings.EnableMod) return;
 
-            _drawer ??= new CellBoolDrawer(this, Find.CurrentMap.Size.x, Find.CurrentMap.Size.z);
+            var map = Find.CurrentMap;
+            if (map == null) return;
+
+            _drawer ??= new CellBoolDrawer(this, map.Size.x, map.Size.z);
 
             _drawer.MarkForDraw();
             if (Find.TickManager.TicksGame >= _nextUpdateTick)
             {
-                UpdateMinMaxValues();
+                UpdateBuildingValues(map);
                 _drawer.SetDirty();
                 _nextUpdateTick = Find.TickManager.TicksGame + updateDelay;
             }
             _drawer.CellBoolDrawerUpdate();
         }
-
+        
         /// <summary>
         /// Gets a boolean value indicating whether the cell at the specified index should be drawn.
         /// </summary>
@@ -51,57 +49,62 @@ namespace MarketValueOverlay
         /// <returns><c>true</c> if the cell should be drawn, <c>false</c> otherwise.</returns>
         public bool GetCellBool(int index)
         {
-            try
+            var map = Find.CurrentMap;
+            if (map == null || map.fogGrid == null || map.edificeGrid == null) return false; // Null checks
+            if (index < 0 || index >= map.cellIndices.NumGridCells) return false; // Index checks
+            if (map.fogGrid.IsFogged(index)) return false; // If the cell is fogged (ex: Ancient Danger)
+
+            var building = map.edificeGrid[index] as Building;
+            if (building == null || building.Faction == null || !building.Faction.IsPlayer) return false; // If the building is owned by a player
+
+            if (_buildingValues.TryGetValue(building.thingIDNumber, out var marketValue))
             {
-                var map = Find.CurrentMap;
-                if (map == null) return false;
-                if (map.fogGrid.IsFogged(index)) return false;
-
-                var building = map.edificeGrid?[index] as Building;
-
-                if (building == null) return false;
-                if (!building.Faction.IsPlayer) return false;
-
-                var marketValue = building.GetStatValue(StatDefOf.MarketValue);
-                if (marketValue > _avgValue)
-                    _color = Color.Lerp(AvgColor, MaxColor, Mathf.InverseLerp(_avgValue, _maxValue, marketValue));
-                else
-                    _color = Color.Lerp(MinColor, AvgColor, Mathf.InverseLerp(_minValue, _avgValue, marketValue));
-
+                _color = GetColorForValue(marketValue);
                 return true;
             }
-            catch
-            {
-                return false;
-            }
+
+            return false;
         }
-
-        /// <summary>
-        /// Gets the extra color for the specified cell.
-        /// </summary>
-        /// <param name="index">The index of the cell to check.</param>
-        /// <returns>The extra color for the cell.</returns>
+        
         public Color GetCellExtraColor(int index) => _color;
-
-        /// <summary>
-        /// Updates the minimum, average and maximum values of market value.
-        /// </summary>
-        private void UpdateMinMaxValues()
+        
+        private void UpdateBuildingValues(Map map)
         {
-            var buildings = Find.CurrentMap.listerBuildings.allBuildingsColonist
-                .Where(b => b.Faction.IsPlayer && b.GetStatValue(StatDefOf.MarketValue) > 0).ToList();
-            if (buildings.Count > 0)
+            _buildingValues.Clear();
+            float sum = 0;
+            var count = 0;
+            _minValue = float.MaxValue;
+            _maxValue = float.MinValue;
+
+            if (map?.listerBuildings?.allBuildingsColonist == null) return;
+
+            foreach (var building in map.listerBuildings.allBuildingsColonist)
             {
-                _minValue = buildings.Min(b => b.GetStatValue(StatDefOf.MarketValue));
-                _maxValue = buildings.Max(b => b.GetStatValue(StatDefOf.MarketValue));
-                _avgValue = buildings.Average(b => b.GetStatValue(StatDefOf.MarketValue));
+                if (building?.Faction == null || !building.Faction.IsPlayer) continue;
+                
+                var value = building.GetStatValue(StatDefOf.MarketValue);
+                if (value > 0)
+                {
+                    _buildingValues[building.thingIDNumber] = value;
+                    sum += value;
+                    count++;
+                    _minValue = Mathf.Min(_minValue, value);
+                    _maxValue = Mathf.Max(_maxValue, value);
+                }
             }
+
+            _avgValue = count > 0 ? sum / count : 0;
+            if (_minValue == float.MaxValue) _minValue = 0;
+            if (_maxValue == float.MinValue) _maxValue = 2000;
+            if (_avgValue == 0) _avgValue = 1000;
+        }
+        
+        private Color GetColorForValue(float value)
+        {
+            if (value > _avgValue)
+                return Color.Lerp(AvgColor, MaxColor, Mathf.InverseLerp(_avgValue, _maxValue, value));
             else
-            {
-                _minValue = 0;
-                _maxValue = 2000;
-                _avgValue = 1000;
-            }
+                return Color.Lerp(MinColor, AvgColor, Mathf.InverseLerp(_minValue, _avgValue, value));
         }
     }
 }
